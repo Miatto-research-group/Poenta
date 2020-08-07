blacked and corrected a factor 1/2

poenta/circuit.py
@@ -15,7 +15,7 @@
 
 import tensorflow as tf
 import numpy as np
-from tqdm.notebook import tqdm
+from rich.progress import track, Progress, BarColumn, TimeRemainingColumn
 from dataclasses import dataclass, field
 from prettytable import PrettyTable
 
@@ -41,7 +41,7 @@ class Circuit:
     def __init__(self, config: Config):
         tf.random.set_seed(config.random_seed)
         np.random.seed(config.random_seed)
-        
+
         self.config = config
         self.state_in = tf.cast(tf.constant(config.state_in), dtype=config.dtype)
         self.objective = tf.cast(tf.constant(config.objective), dtype=config.dtype)
@@ -86,36 +86,44 @@ class Circuit:
 
     def minimize(self) -> list:
         loss_list = []
-        for i in tqdm(range(self.config.steps)):
-            try:
-                loss_list.append(self.minimize_step())
+        with Progress(
+            "[progress.description]{task.description}",
+            BarColumn(),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            TimeRemainingColumn(),
+            f"Fidelity: {100*(self.fidelity):.3f}",
+        ) as bar:
+            task = bar.add_task(description="Optimizing...", total=self.config.steps)
+            for i in range(self.config.steps):
+                try:
+                    loss_list.append(self.minimize_step())
 
-                # LR scheduling
-                for threshold, new_LR in self.config.LR_schedule.items():
-                    if loss_list[-1] < threshold:
-                        self.optimizer.lr = new_LR
+                    # LR scheduling
+                    for threshold, new_LR in self.config.LR_schedule.items():
+                        if loss_list[-1] < threshold:
+                            self.optimizer.lr = new_LR
 
-                if i % 10 == 0:
-                    print(
-                        f"Step {i}/{self.config.steps}: Fidelity = {100*(self.fidelity):.3f}%, LR = {self.optimizer.lr.numpy():.5f}",
-                        end="\r",
-                    )
+                    # if i % 10 == 0:
+                    #     print(
+                    #         f"Step {i}/{self.config.steps}: Fidelity = {100*(self.fidelity):.3f}%, LR = {self.optimizer.lr.numpy():.5f}",
+                    #         end="\r",
+                    #     )
 
-            except KeyboardInterrupt:
-                break
-            except Exception as e:
-                print(f"other exception: {e}")
-                raise e
+                except KeyboardInterrupt:
+                    break
+                except Exception as e:
+                    print(f"other exception: {e}")
+                    raise e
+                bar.advance(task)
         return loss_list
 
     def __repr__(self):
         table = PrettyTable()
-        table.add_column("Layers",[self.config.num_layers])
+        table.add_column("Layers", [self.config.num_layers])
         table.add_column("Cutoff", [self.cutoff])
         table.add_column("Optimizer", [self.config.optimizer])
         trainable_pars = np.sum([p.shape for p in self.parameters.all])
         tot_pars = np.sum([p.shape for p in self.parameters.trainable])
         table.add_column("Params (trainable/tot)", [f"{trainable_pars}/{tot_pars}"])
-        
-        return str(table)
 
+        return str(table)

poenta/jitted.py
@@ -89,8 +89,8 @@ def dC_dmu_dSigma(gamma, phi, z):
         dC_ddelta_over_r = (
             -0.5j * np.conj(gamma) ** 2 * np.exp(1j * (2 * phi + delta)) * (1 - r ** 2 / 3.0 + 2 * r ** 4 / 15.0)
         ) * C
-    dC_dz = np.exp(-1j * delta) * (dC_dr - 1j * dC_ddelta_over_r)
-    dC_dzc = np.exp(1j * delta) * (dC_dr + 1j * dC_ddelta_over_r)
+    dC_dz = np.exp(-1j * delta) * (dC_dr - 1j * dC_ddelta_over_r) / 2
+    dC_dzc = np.exp(1j * delta) * (dC_dr + 1j * dC_ddelta_over_r) / 2
     dC = np.array([dC_dgamma, dC_dgammac, dC_dphi, dC_dz, dC_dzc])
 
     # dmu
@@ -112,8 +112,8 @@ def dC_dmu_dSigma(gamma, phi, z):
         dmu_ddelta_over_r = np.array(
             [1j * np.conj(gamma) * np.exp(1j * (2 * phi + delta)) * (1 - r ** 2 / 3.0 + 2 * r ** 4 / 15.0), 0.0]
         )
-    dmu_dz = np.exp(-1j * delta) * (dmu_dr - 1j * dmu_ddelta_over_r)
-    dmu_dzc = np.exp(1j * delta) * (dmu_dr + 1j * dmu_ddelta_over_r)
+    dmu_dz = np.exp(-1j * delta) * (dmu_dr - 1j * dmu_ddelta_over_r) / 2
+    dmu_dzc = np.exp(1j * delta) * (dmu_dr + 1j * dmu_ddelta_over_r) / 2
     dmu = np.stack((dmu_dgamma, dmu_dgammac, dmu_dphi, dmu_dz, dmu_dzc), axis=-1)
 
     # dSigma
@@ -143,8 +143,8 @@ def dC_dmu_dSigma(gamma, phi, z):
                 [0.0, 1j * np.exp(-1j * delta) * (1 - r ** 2 / 3.0 + 2 * r ** 4 / 15.0)],
             ]
         )
-    dSigma_dz = np.exp(-1j * delta) * (dSigma_dr - 1j * dSigma_ddelta_over_r)
-    dSigma_dzc = np.exp(1j * delta) * (dSigma_dr + 1j * dSigma_ddelta_over_r)
+    dSigma_dz = np.exp(-1j * delta) * (dSigma_dr - 1j * dSigma_ddelta_over_r) / 2
+    dSigma_dzc = np.exp(1j * delta) * (dSigma_dr + 1j * dSigma_ddelta_over_r) / 2
     dSigma = np.stack((dSigma_dgamma, dSigma_dgammac, dSigma_dphi, dSigma_dz, dSigma_dzc), axis=-1)
 
     return dC, dmu, dSigma

poenta/tfutils.py
@@ -20,6 +20,7 @@ from .jitted import G_matrix, grad_newstate, R_matrix
 
 def complex_initializer(dtype):
     f = tf.random_normal_initializer()
+
     def initializer(*args, dtype, **kwargs):
         real = f(*args, **kwargs)
         imag = f(*args, **kwargs)
@@ -27,8 +28,10 @@ def complex_initializer(dtype):
 
     return initializer
 
+
 def real_initializer(dtype):
     f = tf.random_normal_initializer()
+
     def initializer(*args, dtype, **kwargs):
         real = f(*args, **kwargs)
         return tf.cast(real, dtype)
@@ -68,7 +71,9 @@ def GaussianTransformation(gamma, phi, z, Psi):
         grad_gammac = tf.reduce_sum(dy * np.conj(dPsi_dgamma) + tf.math.conj(dy) * dPsi_dgammac)
         grad_phi = 2 * tf.math.real(tf.reduce_sum(dy * np.conj(dPsi_dphi)))
         grad_zc = tf.reduce_sum(dy * np.conj(dPsi_dz) + tf.math.conj(dy) * dPsi_dzc)
-        grad_Psic = tf.linalg.matvec(G, dy, adjoint_a=True) #NOTE: can we compute directly the product between G and dy?
+        grad_Psic = tf.linalg.matvec(
+            G, dy, adjoint_a=True
+        )  # NOTE: can we compute directly the product between G and dy?
         return grad_gammac, grad_phi, grad_zc, grad_Psic
 
     return Psi_new, grad
@@ -84,5 +89,3 @@ def kerr(k, cutoff: int, dtype: tf.dtypes.DType):
     """
     diag = tf.exp(1j * tf.cast(k, dtype=dtype) * np.arange(cutoff) ** 2)
     return diag
-
-