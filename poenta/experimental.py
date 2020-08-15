a93484a64e55e45ff51811d65c7b364861877fb0
ziofil <miatto@gmail.com>
2020-08-15 12:40:26 +0200

    fix

--- poenta/experimental.py
+++ poenta/experimental.py
@@ -19,7 +19,7 @@ from numba import njit
 from .jitted import C_mu_Sigma, dC_dmu_dSigma, convert_scalar
 
 
-@njit(fastmath=True)
+@njit()
 def R_matrix(gamma, phi, z, old_state):
     """
     Directly constructs the transformed state recursively and exactly.
@@ -56,8 +56,11 @@ def R_matrix(gamma, phi, z, old_state):
         R[:, 0, n] = np.sum(old_state * G0[: cutoff - n], axis=-1)
         old_state = old_state[:, 1:] * sqrt[1 : cutoff - n]
 
+    # second row of R matrix
+    R[:, 1, :-1] = mu[0] * R[:, 0, :-1] - Sigma[0, 1] * R[:, 0, 1:]
+        
     # rest of R matrix
-    for m in range(1, cutoff):
+    for m in range(2, cutoff):
         R[:, m, :-m] = (
             mu[0] * R[:, m - 1, :-m]
             - Sigma[0, 0] * sqrt[m - 1] * R[:, m - 2, :-m]
@@ -65,64 +68,6 @@ def R_matrix(gamma, phi, z, old_state):
         ) / sqrt[m]
 
     return R
-
-
-# @njit
-# def grad_newstate(gamma: np.complex, phi: np.float, z: np.complex, psi: np.array, G0: np.array, R: np.array) -> list:
-#     """
-#     Computes the gradient of the new state with respect to
-#     gamma, gamma*, phi, z, z* but not with respect to the old state
-
-#     Arguments:
-#         gamma (complex): displacement parameter
-#         phi (float): phase rotation parameter
-#         z (complex): squeezing parameter
-#         psi: (complex array): old state
-#         G0 (complex array[D]): 1st row of the G matrix
-#         R (complex array[D,D]): complete R matrix
-
-#     Returns:
-#         list[complex array[cutoff]]: gradient of the new state with respect to
-#                                     gamma, gamma*, phi, z, z*
-#     """
-#     batch, cutoff = psi.shape
-#     dtype = psi.dtype
-
-#     z = convert_scalar(z)
-#     phi = convert_scalar(phi)
-#     gamma = convert_scalar(gamma)
-
-#     C, mu, Sigma = C_mu_Sigma(gamma, phi, z)
-#     dC, dmu, dSigma = dC_dmu_dSigma(gamma, phi, z)
-
-#     sqrt = np.sqrt(np.arange(cutoff, dtype=dtype))
-
-#     dR = np.zeros((batch, cutoff, cutoff, 5), dtype=dtype)
-#     dG0 = np.zeros((cutoff, 5), dtype=dtype)
-
-#     # grad of first row of Transformation matrix
-#     dG0[0] = dC
-#     for n in range(cutoff - 1):
-#         dG0[n + 1] = (dmu[1] * G0[n] + mu[1] * dG0[n] - dSigma[1, 1] * sqrt[n] * G0[n - 1] - Sigma[1, 1] * sqrt[n] * dG0[n - 1]) / sqrt[n + 1]
-
-#     # first row of dR matrix
-#     for n in range(cutoff):
-#         dR[:,0, n] = np.dot(psi, dG0[: cutoff - n])
-#         psi = psi[:,1:] * sqrt[1 : cutoff - n]
-
-#     # rest of dR matrix
-#     for m in range(cutoff - 1):
-#         for k in range(cutoff - m - 1):
-#             dR[:,m + 1, k] = (
-#                 np.outer(R[:, m, k], dmu[0])
-#                 + mu[0] * dR[:, m, k]
-#                 - np.outer(R[:, m - 1, k], dSigma[0, 0] * sqrt[m])
-#                 - Sigma[0, 0] * sqrt[m] * dR[:, m - 1, k]
-#                 - Sigma[0, 1] * dR[:, m, k + 1]
-#                 - np.outer(R[:, m, k + 1], dSigma[0, 1])
-#             ) / sqrt[m + 1]
-
-#     return list(np.transpose(dR[:,:, 0], (2, 0, 1)))
 
 
 @njit()
@@ -157,8 +102,16 @@ def dPsi(gamma: np.complex, phi: np.float, z: np.complex, state_in: np.array, G0
         dR[:, 0, n] = np.dot(state_in, dG0[: cutoff - n])
         state_in = state_in[:, 1:] * sqrt[1 : cutoff - n]
 
+    # second row of dR matrix
+    dR[:, 1, :-1] = (
+        ed(R[:, 0, :-1], 2) * ed(ed(dmu[0], 0), 1)
+        - ed(R[:, 0, 1:], 2) * ed(ed(dSigma[0, 1], 0), 0)
+        + mu[0] * dR[:, 0, :-1]
+        - Sigma[0, 1] * dR[:, 0, 1:]
+    )
+    
     # rest of dR matrix
-    for m in range(1, cutoff):
+    for m in range(2, cutoff):
         dR[:, m, :-m] = (
             ed(R[:, m - 1, :-m], 2) * ed(ed(dmu[0], 0), 1)
             - sqrt[m - 1] * ed(R[:, m - 2, :-m], 2) * ed(ed(dSigma[0, 0], 0), 0)
