fixed complex formula

poenta/jitted.py
@@ -32,7 +32,8 @@ def inverse_metric(dpsi_dtheta, dpsi_dthetac, psi, diagonal=False):
     vec = np.dot(dpsi_dtheta, np.conj(psi))
     vecc = np.dot(dpsi_dthetac, np.conj(psi))
     GC = np.dot(np.conj(dpsi_dtheta), dpsi_dtheta.T) - np.outer(np.conj(vec), vec)
-    GCT = np.dot(dpsi_dthetac, np.conj(dpsi_dthetac).T) - np.outer(vecc, np.conj(vec))
+#    GCT = np.dot(dpsi_dthetac, np.conj(dpsi_dthetac).T) - np.outer(vecc, np.conj(vec))
+    GCT = np.dot(dpsi_dthetac, np.conj(dpsi_dthetac).T) - np.outer(np.conj(vec), vec)
     mat = (GC+GCT)/2 + 0.1*np.identity(len(GC))
     inverse = np.linalg.solve(mat, np.identity(len(mat)).astype(psi.dtype)).astype(psi.dtype)
     # print('diagonal: ', np.round(abs(np.diag(inverse)), decimals=2))
@@ -42,8 +43,8 @@ def inverse_metric(dpsi_dtheta, dpsi_dthetac, psi, diagonal=False):
 def inverse_metric_real(dpsi_dtheta, psi):
     vec = np.dot(dpsi_dtheta, np.conj(psi))
     #######Real G##############
-    G = np.real(np.dot(np.conj(dpsi_dtheta), dpsi_dtheta.T) - np.outer(np.conj(vec), vec))
-#    G = np.dot(np.conj(dpsi_dtheta), dpsi_dtheta.T) - np.outer(np.conj(vec), vec)
+#    G = np.real(np.dot(np.conj(dpsi_dtheta), dpsi_dtheta.T) - np.outer(np.conj(vec), vec))
+    G = np.dot(np.conj(dpsi_dtheta), dpsi_dtheta.T) - np.outer(np.conj(vec), vec)
     ######Diag or NOT##########
 #    G = np.diag(np.diagonal(G))
     ###########################